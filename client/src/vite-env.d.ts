9c6dd5773321615e011164e726dd548c6a502774
barock7 <barockandualem7@gmail.com>
2025-11-29 18:30:13 +0300

    3rd update to deploy

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,18 +1,30 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-    readonly VITE_API_URL?: string
-    readonly PROD: boolean
-    readonly DEV: boolean
-    readonly MODE: string
+// Global type augmentations for Vite
+declare global {
+    interface ImportMetaEnv {
+      readonly VITE_API_URL?: string
+      readonly PROD: boolean
+      readonly DEV: boolean
+      readonly MODE: string
+    }
+  
+    interface ImportMeta {
+      readonly env: ImportMetaEnv
+    }
   }
   
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
-  
+  // CSS modules
   declare module '*.css' {
     const content: string
     export default content
   }
+  
+  // SVG modules
+  declare module '*.svg' {
+    const content: string
+    export default content
+  }
+  
+  export {}
   
